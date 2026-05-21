9a3b988b377f6bdc058a4f06d865e1352339c4ff
EkaterinaZaritskaia <137422500+EkaterinaZaritskaia@users.noreply.github.com>
2026-05-21 22:33:15 +0200

    extract data in text file

--- main.py
+++ main.py
@@ -13,10 +13,40 @@ def scrape(url):
 
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["vacancies"]
+    value = extractor.extract(source)["jobs"]
     return value
+
+def load_new_job():
+    """Creation of the .txt file"""
+    try:
+        with open("data.txt", "r") as file:
+            return set(file.read().splitlines())
+    except FileNotFoundError:
+        return set()
+
+def save_new_job(job):
+    """Save data to the .txt file"""
+    with open("data.txt", "a", encoding = "utf-8") as file:
+        file.write(f'{job["title"]} | {job["url"]}\n')
+
+def find_new_jobs(jobs):
+    """Find new vacancy jobs"""
+    seen_jobs = load_new_job()
+
+    new_jobs=[]
+
+    for job in jobs:
+        if job["url"] not in seen_jobs:
+            new_jobs.append(job)
+            save_new_job(job)
+    return new_jobs
 
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+
+    new_jobs = find_new_jobs(extracted)
+    if new_jobs:
+        print("New vacancy found")
+        print(new_jobs)
